cb2807511cf33490a7be4778bcf18d712c754d29
Sevent-77 <rodrigocabral0529@gmail.com>
2023-05-31 08:37:41 -0400

    update day(31/05)

---
+++ app/Libraries/Database.php
@@ -0,0 +1,10 @@
+<?php
+class Database{
+    private $host = "localhost";
+    private $usuario = "root";
+    private $senha = "";
+    private $banco = "blog";
+    private $porta = "3306";
+    private $dbh;
+}
+?>
