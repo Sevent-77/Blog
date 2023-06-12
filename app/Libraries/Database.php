1112e3f127ec2f187024d34271e86563e3f94fc2
Sevent-77 <rodrigocabral0529@gmail.com>
2023-06-12 09:02:49 -0400

    Prova ATT
    Atualização da cor do botão e do database.

--- app/Libraries/Database.php
+++ app/Libraries/Database.php
@@ -1,4 +1,6 @@
 <?php
+// WIP
+/*
 class Database{
     private $host = "localhost";
     private $usuario = "root";
@@ -6,5 +8,14 @@ class Database{
     private $banco = "blog";
     private $porta = "3306";
     private $dbh;
+}*/
+
+try {
+    $dbh = new PDO('mysql:host=localhost;dbname=blog', 'root', '');
+    $dbh = null;
+} catch (PDOException $e) {
+    print "Error!: " . $e->getMessage() . "<br/>";
+    die();
 }
+
 ?>
